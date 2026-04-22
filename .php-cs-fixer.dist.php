58d649f0bdd3eddebcd69c0d7a22ba1861eba981
Natan Felles <natanfelles@gmail.com>
2026-04-22 15:25:40 -0300

    Remove parentheses

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,7 +10,7 @@
 use Framework\CodingStandard\Config;
 use Framework\CodingStandard\Finder;
 
-return (new Config())->setDefaultHeaderComment(
+return new Config()->setDefaultHeaderComment(
     'Aplus Framework',
     'Natan Felles <natanfelles@gmail.com>'
 )->setFinder(
